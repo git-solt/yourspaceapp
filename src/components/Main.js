fe8205feef998541c3faa3f201768a7caae552ee
torje <dev_sol.t@protonmail.com>
2020-11-03 22:21:29 +0100

    added more styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from 'react'
-import {connect} from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
 import Post from '../components/Post'
 import LoadPage from './LoadPage'
 import postSelector from '../hooks/selectors/postSelector'
 import fetchPosts from '../utils/fetchPosts'
-import {addPost} from "../redux/generators/posts";
+import { addPost } from "../redux/generators/posts";
 
 const Main = (props) => {
     let parsed = new DOMParser().parseFromString('&#9679;&#9679;&#9679;', 'text/html')
@@ -42,32 +42,37 @@ const Main = (props) => {
 
     return (
         <div>
-            {loading && <LoadPage/>}
+            {loading && <LoadPage />}
             {postSelector(props.posts, props.filter).map((cur) => <Post dispatch={props.dispatch}
-                                                                        token={props.user.token}
-                                                                        isAdmin={props.user.user.isAdmin}
-                                                                        key={cur.id}{...cur} />)}
-            {props.posts.length > 0 && <p onClick={(e) => {
-                fetchPosts({limit: 5, skip})
-                    .then(data => {
-                        if (data.length > 0) {
-                            data.forEach((cur) => {
-                                const {title, content, published, _id: id, createdAt, pics, owner} = cur
-                                if (!props.posts.some(cur => cur.id === id)) {
-                                    props.dispatch(addPost({title, content, published, id, createdAt, pics, owner}))
-                                    console.log(data)
-                                    setSkip(5)
-                                } else {
-                                    setError('No other posts found')
-                                    setTimeout(() => {
-                                        setError('')
-                                    }, 3000)
-                                }
+                token={props.user.token}
+                isAdmin={props.user.user.isAdmin}
+                key={cur.id}{...cur} />)}
+            {props.posts.length > 0 && (
+                <div className="tooltip container container--text-center tiny link-button--fetch-post">
+                    <p className="max" onClick={(e) => {
+                        fetchPosts({ limit: 5, skip })
+                            .then(data => {
+                                if (data.length > 0) {
+                                    data.forEach((cur) => {
+                                        const { title, content, published, _id: id, createdAt, pics, owner } = cur
+                                        if (!props.posts.some(cur => cur.id === id)) {
+                                            props.dispatch(addPost({ title, content, published, id, createdAt, pics, owner }))
+                                            console.log(data)
+                                            setSkip(5)
+                                        } else {
+                                            setError('No other posts found')
+                                            setTimeout(() => {
+                                                setError('')
+                                            }, 3000)
+                                        }
 
+                                    })
+                                }
                             })
-                        }
-                    })
-            }}>{error || circles}</p>}
+                    }}>{error || circles}</p>
+                    <span className="tooltip--hint">Load more posts</span>
+                </div>)}
+
         </div>
     )
 }
